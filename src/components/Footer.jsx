2852f983a73c1a2b8c2ef5576422eff979304468
Muhammad Awais <111972793+AwaisDotGeek@users.noreply.github.com>
2024-12-04 10:38:04 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="text-center bg-gray-100 py-4">
       <p className="font-bold text-lg">
-        &copy; {new Date().getFullYear()} &lt;AwaisDotGeek /&gt;. All rights reserved.
+        &copy; {new Date().getFullYear()} &lt;Aamir /&gt;. All rights reserved.
       </p>
     </footer>
   );
